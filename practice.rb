b06192e903b3c150e0f9d0f6d8223cdc54ab4ab1
Aziz Kosber <a.kosber@gmail.com>
2018-05-09 17:08:04 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -2,3 +2,19 @@ def input_to_index(user_input)
   user_input = user_input.to_i
   user_input - 1
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    true
+  else
+    false
+  end
+
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      false
+    else
+      true
+    end
+  end
+end
